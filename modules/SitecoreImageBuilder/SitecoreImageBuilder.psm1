7d58035736bddb3fabd3f2700678148fd85aa182
Per Manniche Bering <pmbering@gmail.com>
2019-02-10 19:32:09 +0100

    renamed xconnect image

--- modules/SitecoreImageBuilder/SitecoreImageBuilder.psm1
+++ modules/SitecoreImageBuilder/SitecoreImageBuilder.psm1
@@ -28,7 +28,8 @@ function Invoke-Build
     $defaultPriority = 1000
     $priorities = New-Object System.Collections.Specialized.OrderedDictionary
     $priorities.Add("^sitecore-base:(.*)$", 100)
-    $priorities.Add("^sitecore-openjdk:(.*)$", 200)
+    $priorities.Add("^sitecore-xp-xconnect:(.*)$", 110)
+    $priorities.Add("^sitecore-openjdk:(.*)$", 120)
     $priorities.Add("^(.*)$", $defaultPriority)
     
     # Find out what to build
